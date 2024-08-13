fcc4dbd168373e921682a7fc31a6c234a4c9f325
stevecrawshaw <murrayjarvis@gmail.com>
2024-08-13 15:09:21 +0100

    lnrs species

--- rgbif_species_occ.R
+++ rgbif_species_occ.R
@@ -3,19 +3,26 @@ pacman::p_load(tidyverse,
                janitor,
                fs,
                rgbif,
-               config
+               config,
+               yaml
                )
 
 gbif_creds <- config::get(file = "../config.yml", config = "gbif")
 
 # specified by stuart
-species_tbl <- read_csv2("data/species.csv")
+#species_tbl <- read_csv2("data/species.csv")
+ods_species_path <- "data/lnrs-species-tbl.csv"
+species_tbl <- read_csv(ods_species_path)
+
+
 #https://geopick.gbif.org/
-bounds <- "POLYGON ((-3.065186 51.33576, -2.842712 51.486514, -2.58728 51.633362, -2.183533 51.551459, -2.246704 51.299711, -2.584534 51.218927, -3.043213 51.284253, -3.065186 51.33576))"
+bounds <- "POLYGON (-3.065186 51.33576, -2.842712 51.486514, -2.58728 51.633362, -2.183533 51.551459, -2.246704 51.299711, -2.584534 51.218927, -3.043213 51.284253, -3.065186 51.33576)"
+
+bounds <- "POLYGON ((-3.065186 51.33576, -3.043213 51.284253, -2.584534 51.218927, -2.246704 51.299711, -2.183533 51.551459, -2.58728 51.633362, -2.842712 51.486514, -3.065186 51.33576))"
 # taxonKeys
-keys <- species_tbl$usageKey
+keys <- species_tbl$usage_key
 # set up the preds
-years <- pred_in("year", 2019:2023)
+years <- pred_in("year", 2019:2024)
 area <- pred_within(bounds) #west of england
 taxons <- pred_in("taxonKey", keys)
 
@@ -26,18 +33,24 @@ dl_key <- occ_download(payload,
                        pwd = gbif_creds$pwd, 
                        email = gbif_creds$email)
 
+dl_id <- yaml.load(dl_key)
+
+
 dl_key
 # see how long..
-occ_download_wait('0018392-231120084113126')
+occ_download_wait(dl_id)
 # download
-species_gbif_tbl <- occ_download_get('0018392-231120084113126') %>% 
+species_gbif_tbl <- occ_download_get(dl_id) %>% 
     occ_download_import()
 # inspect
 species_gbif_tbl %>% 
   glimpse()
+
+species_gbif_tbl$usage
 # clean it
 ods_species_tbl <- species_gbif_tbl %>% 
   select(modified,
+         license,
          rightsHolder,
          institutionCode,
          sex, 
@@ -45,7 +58,10 @@ ods_species_tbl <- species_gbif_tbl %>%
          eventDate, year, verbatimLocality, latitude = decimalLatitude,
          longitude = decimalLongitude, scientificName, kingdom, phylum, class,
          order, family, genericName, lastInterpreted, taxonKey, species, level3Name)
+
+unique(ods_species_tbl$license)
+
 # write
 ods_species_tbl %>% 
   clean_names() %>% 
-  write.csv2("data/ods_species_tbl.csv", row.names = FALSE)
+  write.csv2("data/lnrs_species_occ_gbif_tbl.csv", row.names = FALSE, na = "")
